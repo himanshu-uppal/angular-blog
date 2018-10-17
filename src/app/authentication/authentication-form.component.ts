b1b6931bdd26b052d77f1f4e64f252b8690cd904
Himanshu Uppal <himanshu.uppal@nagarro.com>
2018-10-17 12:32:24 +0530

    made authentication file structure for using content projection

---
+++ src/app/authentication/authentication-form.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector:'authentication-form',
+    templateUrl:'./authentication-form.component.html'
+})
+export class AuthenticationFormComponent{
+
+}
--- src/app/core/services/article.service.ts
+++ src/app/core/services/article.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core'
 import {IArticles} from '../models'
 import {Observable} from 'rxjs'
-import {HttpClient} from '@angular/common/http'
+import {HttpClient, HttpHeaders} from '@angular/common/http'
 
 @Injectable()
 export class ArticleService{
@@ -10,7 +10,12 @@ export class ArticleService{
     }
     getArticles():Observable<IArticles>{
         return this.http.get<IArticles>('https://conduit.productionready.io/api/articles')
-
     }
+
+    // saveArticle(article){
+    //     let options = {headers:new HttpHeaders({'Content-Type':'application/json'})}
+
+    //     this.http.post<IArticle>()
+    // }
 
 }
