0b909fb05a7ed3deec4ded8fe7f7eba6ecf822b4
Himanshu Uppal <himanshu.uppal@nagarro.com>
2018-10-18 13:12:49 +0530

    added complete article component

--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from '@angular/core'
 import {CommonModule} from '@angular/common'
 import { ArticleCreateComponent } from './article-create.component';
+import {ArticleRoutingModule} from './article-routing.module'
 
 @NgModule({
     declarations:[ArticleCreateComponent],
-    imports:[CommonModule]
+    imports:[CommonModule,ArticleRoutingModule]
 
 })
 export class ArticleModule{
