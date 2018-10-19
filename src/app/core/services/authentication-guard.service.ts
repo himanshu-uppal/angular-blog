e241b5cd28927ee94f6b35468f132806e709496b
Himanshu Uppal <himanshu.uppal@nagarro.com>
2018-10-19 15:17:42 +0530

    added authentication guard service

--- src/app/core/services/authentication-guard.service.ts
+++ src/app/core/services/authentication-guard.service.ts
@@ -1,6 +1,18 @@
 import {Injectable} from '@angular/core'
+import { AuthenticationService } from './authentication.service';
+import {CanActivate,Router} from '@angular/router'
 
 @Injectable()
-export class AuthenticationGuardService{
+export class AuthenticationGuardService implements CanActivate{
+
+    constructor(private authenticationService:AuthenticationService,private router:Router){}
+
+    canActivate(){
+        if(this.authenticationService.isAuthenticated()){
+            return true
+        }
+        this.router.navigate(['/login'])        
+        }
+
    
 }
