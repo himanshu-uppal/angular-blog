60c5b7da811b6b3b7ddd4c76830f120bc11006e4
Himanshu Uppal <himanshu.uppal@nagarro.com>
2018-10-20 11:27:09 +0530

    added auth guard for creating or updating an article

--- src/app/article/article-routing.module.ts
+++ src/app/article/article-routing.module.ts
@@ -6,10 +6,11 @@ import { ArticleCreateComponent } from './article-create.component';
 import { ArticleUpdateComponent } from './article-update.component';
 import { ArticleResolverService } from './article-resolver.service';
 import { ArticleComponent } from './article.component';
+import {AuthenticationGuardService} from '../core'
 
 const routes:Routes = [
-    {path:'new',component:ArticleCreateComponent},
-    {path:'edit/:slug',component:ArticleUpdateComponent,resolve:{article:ArticleResolverService}},
+    {path:'new',component:ArticleCreateComponent,canActivate:[AuthenticationGuardService]},
+    {path:'edit/:slug',component:ArticleUpdateComponent,resolve:{article:ArticleResolverService},canActivate:[AuthenticationGuardService]},
     {path:':slug',component:ArticleComponent,resolve:{article:ArticleResolverService}}
 ]
 @NgModule({
--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -7,13 +7,14 @@ import {FormsModule} from '@angular/forms'
 import {ArticleService} from '../core'
 import {ArticleResolverService} from './article-resolver.service'
 import {ArticleComponent} from './article.component'
+import {AuthenticationGuardService} from '../core'
 
 @NgModule({
     declarations:[ArticleCreateComponent,ArticleUpdateComponent,ArticleComponent],
     imports:[CommonModule,ArticleRoutingModule,FormsModule],
-    providers:[ArticleService,ArticleResolverService]
+    providers:[ArticleService,ArticleResolverService,AuthenticationGuardService]
 
 })
 export class ArticleModule{
 
-}
+} 
